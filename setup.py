b0c6c858fa7248268804c222ccc927c47558c872
eser-chr <chriseseroglou@gmail.com>
2025-04-04 23:06:41 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from skbuild import setup
+
+setup(
+    name="tasep",     # The name of the wheel on PyPI or when you install via pip
+    version="0.1.0",
+    description="An engine to simulate kinesins",
+)
